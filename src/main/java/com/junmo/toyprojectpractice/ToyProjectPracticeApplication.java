6c6904e22290ecf0c1a08b45deee51353d1e2e8d
junmo95 <junmop950@gmail.com>
2023-10-29 13:17:49 +0900

    Feat: 프로젝트 생성

---
+++ src/main/java/com/junmo/toyprojectpractice/ToyProjectPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.junmo.toyprojectpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyProjectPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToyProjectPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/junmo/toyprojectpractice/ToyProjectPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junmo.toyprojectpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyProjectPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
